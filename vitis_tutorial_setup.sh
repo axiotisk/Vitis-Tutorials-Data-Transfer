0118b4dabb0ebd6b8f70a1e59621dcec3a6a127c
axiotisk <konstantinos.axiotis@cern.ch>
2022-04-22 13:53:14 +0200

    vitis setup script

---
+++ vitis_tutorial_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Set's up the environment variables to run the Vitis-Tutorial
+
+source /opt/xilinx/xrt/setup.sh
+source /home/xilinx/Vitis/2021.2/settings64.sh
+
+PLATFORM_REPO_PATHS=/opt/xilinx/platforms/
+LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
+CPATH=/usr/include/x86_64-linux-gnu
+
+export PLATFORM_REPO_PATHS
+export LIBRARY_PATH
+export CPATH
