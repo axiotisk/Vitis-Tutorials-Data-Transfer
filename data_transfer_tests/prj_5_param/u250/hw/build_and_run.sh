a81e0c19b5a456dffeb42e892c17c5fe181d86ec
axiotisk <konstantinos.axiotis@cern.ch>
2022-06-28 11:54:38 +0200

    Setting build and run for param test

--- data_transfer_tests/prj_5_param/u250/hw/build_and_run.sh
+++ data_transfer_tests/prj_5_param/u250/hw/build_and_run.sh
@@ -3,8 +3,30 @@
 # Exit when any command fails
 set -e
 
-make clean
+#Set by the user
+BUF_SIZE=1000000
+NUM_BUF=2
 
+CSV_DIR=csv_files/
+SUMMARY_DIR=summaries/
+KEY_EXE_TIMES_DIR=key_exe_times/
+
+TARGET_DIR=nb_${NUM_BUF}/
+
+#Full paths to directory
+KEY_EXE_TIMES_SAVING_DIR=${CSV_DIR}${KEY_EXE_TIMES_DIR}${TARGET_DIR}
+SUMMARY_SAVING_DIR=${CSV_DIR}${SUMMARY_DIR}${TARGET_DIR}
+
+#Deleting saving directories if they exist
+if [ -d "$KEY_EXE_TIMES_SAVING_DIR" ]; then rm -Rf $KEY_EXE_TIMES_SAVING_DIR; fi
+if [ -d "$SUMMARY_SAVING_DIR" ]; then rm -Rf $SUMMARY_SAVING_DIR; fi
+
+#Creating the directories
+mkdir -p $KEY_EXE_TIMES_SAVING_DIR
+mkdir -p $SUMMARY_SAVING_DIR
+
+#make clean
+#
 # Make sure everything is up to date
 make -j
 
@@ -12,8 +34,11 @@ make -j
 ## Check if XCL_EMULATION_MODE is set and if os then unset it before running
 ## the executatble
 if [[ -z "${XCL_EMULATION_MODE}" ]]; then
-  ./app.exe 
+  ./app.exe ${KEY_EXE_TIMES_SAVING_DIR} ${BUF_SIZE} 
 else
   unset XCL_EMULATION_MODE
-  ./app.exe 
+  ./app.exe ${KEY_EXE_TIMES_SAVING_DIR} ${BUF_SIZE} 
 fi
+
+#Coping summary file to folder
+cp summary.csv ${SUMMARY_SAVING_DIR}/bs_${BUF_SIZE}.csv
