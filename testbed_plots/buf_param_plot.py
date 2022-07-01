55cd70e7ab7ec29f5cd19ac8e847e593505925bf
kaxiotis <konstantinos.axiotis@cern.ch>
2022-07-01 16:53:07 +0200

    Buf param plot to script

---
+++ testbed_plots/buf_param_plot.py
@@ -0,0 +1,17 @@
+import os             
+import sys
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+import argparse
+
+from key_exe_times import *
+
+path = "./key_exe_times_csv/buf_param.csv"
+
+csv_values = openclTrace()
+csv_values.csv_reader(path)
+csv_values.duration_calc()
+
+csv_values.plot_times()
