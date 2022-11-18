451ea78867af8783e2091d5b64c677eebd802751
Konstantinos Axiotis <kaxiotis@at-tdaq-mldev02.dyndns.cern.ch>
2022-11-18 11:10:22 +0100

    Prj_5_loop on versal

---
+++ data_transfer_tests/versal/prj_5_loop/u250/hw/build_and_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Exit when any command fails
+set -e
+
+#make clean
+
+# Make sure everything is up to date
+make
+
+# Run the application in HW emulation mode
+## Check if XCL_EMULATION_MODE is set and if os then unset it before running
+## the executatble
+if [[ -z "${XCL_EMULATION_MODE}" ]]; then
+  ./app.exe 
+else
+  unset XCL_EMULATION_MODE
+  ./app.exe 
+fi
